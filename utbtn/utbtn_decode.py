4f91cfa238edca878d620e2b7a2e103c17af43bd
Paul Kim <pauljoohyunkim@gmail.com>
2024-09-17 23:30:12 +0900

    Left and right edge detection

--- utbtn/utbtn_decode.py
+++ utbtn/utbtn_decode.py
@@ -3,6 +3,7 @@ import numpy as np
 from utbtnlib import UTBTN_Images, A4_SIZE, SQUARE_WIDTH, L_MARGIN, R_MARGIN, T_MARGIN, B_MARGIN
 from typing import Union
 import bz2
+#import pdb
 
 # if output is a string, then it will be saved as a file
 # if kept as None, then it will return the bytes
@@ -40,10 +41,38 @@ def process_images_to_utbtn_images(raw_images : list, n_bytes, size=A4_SIZE, squ
     return images
 
 # Due to compression, it is unlikely that a whole row or column is zero for usual files.
-def detect_box(image : np.ndarray, pixel_threshold=150):
+def detect_box(image : np.ndarray, pixel_threshold=150, ignore_first=10):
     n_row, n_col = image.shape
-    thresholded_image = np.vectorize(lambda x : 1 if x < pixel_threshold else 0)(image).astype(np.uint8)
+    # For computation, black = 1, white = 0
+    thresholded_image = np.vectorize(lambda x : 1 if x < pixel_threshold else 0)(image).astype(np.float16)
 
-    # Detect left edge
+    # Detect left and right edge
+    left_kernel = np.zeros((n_row, 3))
+    left_kernel[:,0] = -1
+    left_kernel[:,2] = 1
+    left_convolutions = [0] + [weighted_sum(left_kernel, thresholded_image[:,i-1:i+2]) for i in range(1, n_col-1)] + [0]
+    for i in range(n_col):
+        if i < ignore_first:
+            continue
+        if left_convolutions[i] != 0:
+            break
+    l_margin = i+1
+    for j in range(n_col):
+        if j < ignore_first:
+            continue
+        if left_convolutions[n_col-j-1] != 0:
+            break
+    r_margin = j+1
 
+    # Detect top and bottom edge
+    top_kernel = np.zeros((3, n_col))
+    top_kernel[0,:] = -1
+    top_kernel[2,:] = 1
+    top_convolutions = [0] + [weighted_sum(top_kernel, thresholded_image[i-1:i+2,:]) for i in range(1, n_row-1)] + [0]
+
+    return (l_margin, r_margin)
+
+def weighted_sum(kernel : np.ndarray, mat : np.ndarray):
+    return np.dot(kernel.flatten(), mat.flatten())
+    
 
